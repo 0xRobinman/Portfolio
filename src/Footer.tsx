4e6b99bf9198517bd7e5098fb563cae2816e965a
0liverRobinson <190023113+0xRobinman@users.noreply.github.com>
2025-01-05 00:22:21 +0000

    matrix bg

---
+++ src/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+
+// At the bottom show 'Powered by GitHub Pages' Link to the repo
+function Footer() {
+    return (
+        <footer>
+            <p>© 2021</p>
+        </footer>
+    )
+}
